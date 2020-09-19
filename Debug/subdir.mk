34912c1476839bbfd3f47fdfd4564faef901421d
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2020-09-19 23:38:53 +1000

    complete. Need to tidy.

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,36 +4,40 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CU_SRCS += \
-../MandelbrotCUDA.cu 
+../mandelbrotCUDA.cu 
 
 C_SRCS += \
 ../bmpfile.c 
 
+O_SRCS += \
+../bmpfile.o \
+../mandelbrotCUDA.o 
+
 OBJS += \
-./MandelbrotCUDA.o \
-./bmpfile.o 
+./bmpfile.o \
+./mandelbrotCUDA.o 
 
 CU_DEPS += \
-./MandelbrotCUDA.d 
+./mandelbrotCUDA.d 
 
 C_DEPS += \
 ./bmpfile.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.cu
-	@echo 'Building file: $<'
-	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-10.2/bin/nvcc -G -g -O0 -gencode arch=compute_70,code=sm_70  -odir "." -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-10.2/bin/nvcc -G -g -O0 --compile --relocatable-device-code=false -gencode arch=compute_70,code=compute_70 -gencode arch=compute_70,code=sm_70  -x cu -o  "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
 	/usr/local/cuda-10.2/bin/nvcc -G -g -O0 -gencode arch=compute_70,code=sm_70  -odir "." -M -o "$(@:%.o=%.d)" "$<"
 	/usr/local/cuda-10.2/bin/nvcc -G -g -O0 --compile  -x c -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-10.2/bin/nvcc -G -g -O0 -gencode arch=compute_70,code=sm_70  -odir "." -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-10.2/bin/nvcc -G -g -O0 --compile --relocatable-device-code=false -gencode arch=compute_70,code=compute_70 -gencode arch=compute_70,code=sm_70  -x cu -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- mandelbrotCUDA.cu
+++ mandelbrotCUDA.cu
@@ -4,29 +4,176 @@
 
 // Define constants
 #define VERBOSE 1
-#define FILENAME "my_mandelbrot_fractal.bmp"
-
-/*Mandelbrot values*/
-#define RESOLUTION 8700.0
-#define XCENTER -0.55
-#define YCENTER 0.6
-#define MAX_ITER 1000
-#define MIN_WIDTH 100
-#define MAX_WIDTH 19200
-#define MIN_HEIGHT 100
-#define MAX_HEIGHT 10800
-
-// Define success and failure return values
 #define SUCCESS 1
 #define FAILURE 0
 
-/*Colour Values*/
+// Mandelbrot constants
+#define RESOLUTION 8700.0	// fractal resolution/detail
+#define XCENTER -0.55
+#define YCENTER 0.6
+#define MAX_ITER 1000
+#define MIN_WIDTH 100		// min width of output image
+#define MAX_WIDTH 19200		// max width of output image
+#define MIN_HEIGHT 100		// min height of output image
+#define MAX_HEIGHT 10800	// max height of output image
+#define COLOURS 3 			// three for the RGB colour space
+#define FILENAME "my_mandelbrot_fractal.bmp"
+#define BYTES_IN_KB 1024
+
+// Colour constants
+#define BIT_DEPTH 32
 #define COLOUR_DEPTH 255
 #define COLOUR_MAX 240.0
 #define GRADIENT_COLOUR_MAX 230.0
 
+// CUDA constants
+#define THREADS_PER_BLOCK 128
+
 // function definitions - see below main() for function body
 int parseArgs(int argc, char *argv[], int *width, int *height);
+
+void GroundColorMix(double* color, double x, double min, double max);
+
+__global__ void mandelbrot(double *pixels, const int width, const int height,
+		const int numPixels, const int xoffset, const int yoffset);
+
+/* Mandelbrot Set Image Demonstration
+ *
+ * This is a simple single-process/single thread implementation
+ * that computes a Mandelbrot set and produces a corresponding
+ * Bitmap image. The program demonstrates the use of a colour
+ * gradient
+ *
+ * This program uses the algorithm outlined in:
+ *   "Building Parallel Programs: SMPs, Clusters And Java", Alan Kaminsky
+ *
+ * This program requires libbmp for all bitmap operations.
+ *
+ */
+
+int main(int argc, char **argv) {
+
+	// Initialize CL arg variables
+	int width, height, totalPixels;
+
+	// parse CL args
+	if (!parseArgs(argc, argv, &width, &height)) {
+		exit(EXIT_FAILURE);
+	}
+
+	// Calculate image size and offset
+	totalPixels = width * height;
+	size_t elements = totalPixels;
+	size_t size = totalPixels * sizeof(double);
+	int xoffset = -(width - 1) / 2;
+	int yoffset = (height - 1) / 2;
+
+	if (VERBOSE) {
+		fprintf(stderr, "Creating a fractal of size %ipx x %ipx\n", width, height);
+		fprintf(stderr, "Total pixels: %i million (%i)\n",totalPixels/1000000, totalPixels);
+		fprintf(stderr, "Total memory required: %ld MB (%ld bytes)\n", size/BYTES_IN_KB/BYTES_IN_KB, size);
+	}
+
+	// Allocate the host array
+	double *h_Pixels = (double *) malloc(size);
+
+	// Allocate the device array
+	double *d_Pixels = NULL;
+	cudaMalloc((void **) &d_Pixels, size);
+
+	// Launch the VectorAdd CUDA Kernel
+	int blocksPerGrid = (elements + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+	if (VERBOSE) {
+		fprintf(stderr, "CUDA kernel launch with %d blocks of %d threads\n",
+				blocksPerGrid,
+				THREADS_PER_BLOCK);
+	}
+
+	mandelbrot<<<blocksPerGrid, THREADS_PER_BLOCK>>>(d_Pixels, width, height,
+			totalPixels,xoffset,yoffset);
+
+	// Copy data back from device
+	if (VERBOSE) {
+		fprintf(stderr,
+				"Copy output data from the CUDA device to the host memory\n");
+	}
+
+	cudaMemcpy(h_Pixels, d_Pixels, size, cudaMemcpyDeviceToHost);
+
+	// Initialize the bitmap variables
+	bmpfile_t *bmp;
+	rgb_pixel_t pixel = { 0, 0, 0, 0 };
+	bmp = bmp_create(width, height, BIT_DEPTH);
+	double color[3];
+	int col,row;
+
+	// colour pixels based on Mandelbrot values
+	if (VERBOSE) {
+		fprintf(stderr,
+				"Colouring pixels and generating bitmap\n");
+	}
+
+	for (int i = 0; i < totalPixels; i++) {
+
+		col = i % (width-1);
+		row = i / (width-1);
+		GroundColorMix(color, h_Pixels[i], 1, COLOUR_DEPTH);
+		pixel.red = color[0];
+		pixel.green = color[1];
+		pixel.blue = color[2];
+		bmp_set_pixel(bmp, col, row, pixel);
+	}
+
+
+	//save bitmap
+	bmp_save(bmp, FILENAME);
+	bmp_destroy(bmp);
+
+	fprintf(stderr, "Complete!\nFractal saved to file: ./%s\n", FILENAME);
+
+
+	// Free memory and exit
+	cudaFree(d_Pixels);
+	free(h_Pixels);
+	exit(EXIT_SUCCESS);
+}
+
+/*
+ * Parse and validate command line args
+ *
+ * Arguments:
+ * int argc - number of arguments supplied
+ * char *argv[] - array of command line arguments
+ * int *width - width of output file
+ * int *height - height of output file
+ *
+ * Return: FAILURE or SUCCESS values defined above
+ */
+int parseArgs(int argc, char *argv[], int *width, int *height) {
+	// validate the number of args
+	if (argc == 3) { // parse and validate args
+
+		// parse and validate the image width
+		if ((*width = atoi(argv[1])) < MIN_WIDTH || *width > MAX_WIDTH) {
+			fprintf(stderr, "Width of image must be between %i and %i\n",
+			MIN_WIDTH, MAX_WIDTH);
+			return (FAILURE);
+		}
+
+		// parse and validate the image height
+		if ((*height = atoi(argv[2])) < MIN_HEIGHT || *height > MAX_HEIGHT) {
+			fprintf(stderr, "Height of image must be between %i and %i\n",
+			MIN_HEIGHT, MAX_HEIGHT);
+			return (FAILURE);
+		}
+
+	} else { // incorrect number of args supplied
+		fprintf(stderr, "Usage: %s image_width image_height\n", argv[0]);
+		return (FAILURE);
+	}
+
+	return (SUCCESS);
+}
 
 /**
  * Computes the color gradient
@@ -77,127 +224,55 @@ void GroundColorMix(double* color, double x, double min, double max) {
 	}
 }
 
-/* Mandelbrot Set Image Demonstration
- *
- * This is a simple single-process/single thread implementation
- * that computes a Mandelbrot set and produces a corresponding
- * Bitmap image. The program demonstrates the use of a colour
- * gradient
- *
- * This program uses the algorithm outlined in:
- *   "Building Parallel Programs: SMPs, Clusters And Java", Alan Kaminsky
- *
- * This program requires libbmp for all bitmap operations.
- *
+/**
+ * Computes the mandelbrot stuff
  */
+__global__ void
 
-int main(int argc, char **argv) {
+mandelbrot(double *pixels, const int width, const int height,
+		const int numPixels, const int xoffset, const int yoffset) {
 
-	int width, height;
+	// calculate first pixel for this thread
+	long i = blockDim.x * blockIdx.x + threadIdx.x;
 
-	// parse CL args
-	if (!parseArgs(argc, argv, &width, &height)) {
-		exit(EXIT_FAILURE);
-	}
+	// calculate mandelbrot set for all pixels assigned to this thread
+	while (i < numPixels) {
 
-	fprintf(stderr, "Creating a fractal: %ipx x %ipx\n",width, height);
+		// work out the x/y position of this pixel
+		int xPosition = i % (width-1);
+		int yPosition = i / (width-1);
 
+		//Determine where in the mandelbrot set, the pixel is referencing
+		double x = XCENTER + (xoffset + xPosition) / RESOLUTION;
+		double y = YCENTER + (yoffset - yPosition) / RESOLUTION;
 
-	bmpfile_t *bmp;
-	rgb_pixel_t pixel = { 0, 0, 0, 0 };
-	int xoffset = -(width - 1) / 2;
-	int yoffset = (height - 1) / 2;
-	bmp = bmp_create(width, height, 32);
-	int col = 0;
-	int row = 0;
-	for (col = 0; col < width; col++) {
-		for (row = 0; row < height; row++) {
+		//Mandelbrot stuff
 
-			//Determine where in the mandelbrot set, the pixel is referencing
-			double x = XCENTER + (xoffset + col) / RESOLUTION;
-			double y = YCENTER + (yoffset - row) / RESOLUTION;
+		double a = 0;
+		double b = 0;
+		double aold = 0;
+		double bold = 0;
+		double zmagsqr = 0;
+		int iter = 0;
 
-			//Mandelbrot stuff
+		//Check if the x,y coord are part of the mendelbrot set - refer to the algorithm
+		while (iter < MAX_ITER && zmagsqr <= 4.0) {
+			++iter;
+			a = (aold * aold) - (bold * bold) + x;
+			b = 2.0 * aold * bold + y;
 
-			double a = 0;
-			double b = 0;
-			double aold = 0;
-			double bold = 0;
-			double zmagsqr = 0;
-			int iter = 0;
-			double x_col;
-			double color[3];
-			//Check if the x,y coord are part of the mendelbrot set - refer to the algorithm
-			while (iter < MAX_ITER && zmagsqr <= 4.0) {
-				++iter;
-				a = (aold * aold) - (bold * bold) + x;
-				b = 2.0 * aold * bold + y;
+			zmagsqr = a * a + b * b;
 
-				zmagsqr = a * a + b * b;
+			aold = a;
+			bold = b;
 
-				aold = a;
-				bold = b;
-
-			}
-
-			/* Generate the colour of the pixel from the iter value */
-			/* You can mess around with the colour settings to use different gradients */
-			/* Colour currently maps from royal blue to red */
-			x_col =
+			pixels[i] =
 					(COLOUR_MAX
 							- ((((float) iter / ((float) MAX_ITER)
 									* GRADIENT_COLOUR_MAX))));
-			GroundColorMix(color, x_col, 1, COLOUR_DEPTH);
-			pixel.red = color[0];
-			pixel.green = color[1];
-			pixel.blue = color[2];
-			bmp_set_pixel(bmp, col, row, pixel);
-
 		}
 
+		// iterate to next pixel
+		i += blockDim.x * gridDim.x;
 	}
-
-	bmp_save(bmp, FILENAME);
-	bmp_destroy(bmp);
-
-	fprintf(stderr, "Complete!\nFractal saved to file: ./%s\n",FILENAME);
-
-	return 0;
-}
-
-/*
- * Parse and validate command line args
- *
- * Arguments:
- * int argc - number of arguments supplied
- * char *argv[] - array of command line arguments
- * int *width - width of output file
- * int *height - height of output file
- *
- * Return: FAILURE or SUCCESS values defined above
- */
-int parseArgs(int argc, char *argv[], int *width, int *height) {
-	// validate the number of args
-	if (argc == 3) { // parse and validate args
-
-		// parse and validate the image width
-		if ((*width = atoi(argv[1])) < MIN_WIDTH || *width > MAX_WIDTH) {
-			fprintf(stderr, "Width of image must be between %i and %i\n",
-			MIN_WIDTH, MAX_WIDTH);
-			return (FAILURE);
-		}
-
-		// parse and validate the image height
-		if ((*height = atoi(argv[2])) < MIN_HEIGHT || *height > MAX_HEIGHT) {
-			fprintf(stderr, "Height of image must be between %i and %i\n",
-			MIN_HEIGHT, MAX_HEIGHT);
-			return (FAILURE);
-		}
-
-	} else { // incorrect number of args supplied
-		fprintf(stderr, "Usage: %s image_width image_height\n", argv[0]);
-		return (FAILURE);
-	}
-
-	return (SUCCESS);
 }
